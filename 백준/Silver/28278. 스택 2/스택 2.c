090152272fdbdda03df6d6abf3e2048ddfd8dd48
hylim <143856509+limHyeonTaek@users.noreply.github.com>
2024-12-10 02:38:11 +0900

    [Silver IV] Title: 스택 2, Time: 216 ms, Memory: 4900 KB -BaekjoonHub

---
+++ 백준/Silver/28278. 스택 2/스택 2.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_SIZE 1000000
+
+typedef struct {
+    int arr[MAX_SIZE];
+    int top;
+} Stack;
+
+void init(Stack *s) {
+    s->top = -1;
+}
+
+void push(Stack *s, int x) {
+    s->arr[++(s->top)] = x;
+}
+
+int pop(Stack *s) {
+    if (s->top == -1) return -1;
+    return s->arr[(s->top)--];
+}
+
+int size(Stack *s) {
+    return s->top + 1;
+}
+
+int empty(Stack *s) {
+    return s->top == -1 ? 1 : 0;
+}
+
+int top(Stack *s) {
+    if (s->top == -1) return -1;
+    return s->arr[s->top];
+}
+
+int main() {
+    Stack s;
+    init(&s);
+    int N, cmd, x;
+    scanf("%d", &N);
+    while (N--) {
+        scanf("%d", &cmd);
+        switch(cmd) {
+            case 1:
+                scanf("%d", &x);
+                push(&s, x);
+                break;
+            case 2:
+                printf("%d\n", pop(&s));
+                break;
+            case 3:
+                printf("%d\n", size(&s));
+                break;
+            case 4:
+                printf("%d\n", empty(&s));
+                break;
+            case 5:
+                printf("%d\n", top(&s));
+                break;
+        }
+    }
+    return 0;
+}
